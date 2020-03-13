9ad67a42fc4c66b54b78f01517aa0c0f4db1f8ea
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2020-03-13 23:24:22 +0530

    updated by imshahnawazshaikh

--- Inheritance1.java
+++ Inheritance1.java
@@ -1,11 +1,12 @@
-class Parent
-{
-	 Parent(){
+class Parent{
+	 Parent(int var){
+        System.out.println("Variable passed by child constructor "+var);
        System.out.println("Parent="+this);
 	 }
 }
 public class Inheritance1 extends Parent{
 	 Inheritance1(){
+		 super(30);
        System.out.println("Child="+this);
 	 }
 	 public static void main(String...x){
